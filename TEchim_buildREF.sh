059004596e08b901d41ea0c77a763c6fb7e9ab0b
charliefornia <charliefornia@me.com>
2019-05-22 16:46:09 +0100

    faster generation of _INTRON.gtf

--- TEchim_buildREF.sh
+++ TEchim_buildREF.sh
@@ -71,37 +71,35 @@ cat $REFbase".gtf" | tr ';' '\t' | awk 'BEGIN {OFS = "\t"} {gsub(/\"/,"",$10); i
 # exons of the same transcript.
 # extract all lines that describe an "exon" in input gtf
 cat $REFbase".gtf" | awk 'BEGIN {OFS = "\t"} {if ($3 == "exon") {print$0}}' > "tmp."$REFbase".exons.gtf"
-
 # extract a (unique) list of all transcript names in input gtf
 cat $REFbase".gtf" | tr ';' '\t' | awk 'BEGIN {OFS = "\t"} {gsub(/\"/,"",$14); if ($3 == "exon") {print$14}}' | awk '!seen[$0]++' > "tmp."$REFbase".exons.transcripts.gtf"
-
 # make sure that file does not yet exist in the current directory
-rm -f $REFbase".output.introns.gtf"
-
+rm -f $REFbase"_INTRONS.gtf"
 # loop through each transcript in gtf file
 while read line
 do
+	LC_ALL=C fgrep $line "tmp."$REFbase".exons.gtf" > "tmp."$REFbase".input.list"
 	# if clause distinguishes between transcripts on the positive- and negative strand
-	if [[ $(fgrep $line "tmp."$REFbase".exons.gtf" | head -n1 | awk '{print$7}') == "+" ]]
+	if [[ $(cat "tmp."$REFbase".input.list" | head -n1 | awk '{print$7}') == "+" ]]
 	then
 		# on the positive strand, the start of the first intron is the end of the first exon minus one.
 		# note that the last line of exons is deleted. this is because there's always one intron less than there are exons.
-		LC_ALL=C fgrep $line "tmp."$REFbase".exons.gtf" | awk '{print $1"\t"$2"\tintron\t"$5+1}' | sed '$d' > "tmp."$REFbase".cola.tsv"
+		cat "tmp."$REFbase".input.list" | awk '{print $1"\t"$2"\tintron\t"$5+1}' | sed '$d' > "tmp."$REFbase".cola.tsv"
 		# on the positive strand, the end of introns are the start of exons minus one.
 		# note that here the first line is removed, not the last one. as a consequence, the values are shifted up one line.
-		LC_ALL=C fgrep $line "tmp."$REFbase".exons.gtf" | awk 'NR>1 {print$4-1}' > "tmp."$REFbase".colb.tsv"
+		cat "tmp."$REFbase".input.list" | awk 'NR>1 {print$4-1}' > "tmp."$REFbase".colb.tsv"
 		# the remaining information about gene- and transcript name and id are added.
-		LC_ALL=C fgrep $line "tmp."$REFbase".exons.gtf" | awk '{print $6"\t"$7"\t"$8"\t"$9"; "$10"; "$11"; "$12"; "$13"; "$14"; "$15"; "$16}' | sed '$d'  > "tmp."$REFbase".colc.tsv"
+		cat "tmp."$REFbase".input.list" | awk '{print $6"\t"$7"\t"$8"\t"$9"; "$10" "$11"; "$12" "$13"; "$14" "$15"; "$16}' | sed '$d'  > "tmp."$REFbase".colc.tsv"
 		paste "tmp."$REFbase".cola.tsv" "tmp."$REFbase".colb.tsv" "tmp."$REFbase".colc.tsv" >> $REFbase"_INTRONS.gtf"
 		rm "tmp."$REFbase".col"*
 	elif [[ $(fgrep $line "tmp."$REFbase".exons.gtf" | head -n1 | awk '{print$7}') == "-" ]]
 	then
 		# on the negative strand, the entries are stacked by removing the first line of the beginning and end of the entries.
-		LC_ALL=C fgrep $line "tmp."$REFbase".exons.gtf" | awk 'NR>1 {print $1"\t"$2"\tintron\t"$5+1}' > "tmp."$REFbase".cola.tsv"
+		cat "tmp."$REFbase".input.list" | awk 'NR>1 {print $1"\t"$2"\tintron\t"$5+1}' > "tmp."$REFbase".cola.tsv"
 		# on the negative strand, the beginning of introns are the start of the exon minus one.
-		LC_ALL=C fgrep $line "tmp."$REFbase".exons.gtf" | awk '{print$4-1}' | sed '$d' > "tmp."$REFbase".colb.tsv"
+		cat "tmp."$REFbase".input.list" | awk '{print$4-1}' | sed '$d' > "tmp."$REFbase".colb.tsv"
 		# on the negative strand, the end of exons are the beginning of the trailing exon plus one.
-		LC_ALL=C fgrep $line "tmp."$REFbase".exons.gtf" | awk 'NR>1 {print $6"\t"$7"\t"$8"\t"$9"; "$10"; "$11"; "$12"; "$13"; "$14"; "$15"; "$16}'   > "tmp."$REFbase".colc.tsv"
+		cat "tmp."$REFbase".input.list" | awk 'NR>1 {print $6"\t"$7"\t"$8"\t"$9"; "$10" "$11"; "$12" "$13"; "$14" "$15"; "$16}'   > "tmp."$REFbase".colc.tsv"
 		paste "tmp."$REFbase".cola.tsv" "tmp."$REFbase".colb.tsv" "tmp."$REFbase".colc.tsv" >> $REFbase"_INTRONS.gtf"
 		rm "tmp."$REFbase".col"*
 	else
