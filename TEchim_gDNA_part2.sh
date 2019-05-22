9192d4daf198e684a0ad99f82bdd8ee5cb21bb7a
charliefornia <charliefornia@me.com>
2019-05-22 16:46:40 +0100

    fixed strandedness issue

---
+++ TEchim_gDNA_part2.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+################################################################################
+# TITLE: TEchim - part 2
+# VERSION: 0.1.1 (dev)
+# AUTHOR: Christoph Treiber, Waddell lab, University of Oxford
+# DATE: 09/05/2019 (dd/mm/yyyy)
+# DESCRIPTION: tbd
+################################################################################
+
+################################################################################
+# REQUIREMENTS:
+# - bedtools
+################################################################################
+
+################################################################################
+################################################################################
+# set parameters
+wd=~
+path_to_PART1_output=/PATH/TO/PART1/OUTPUT/
+SNa=NAME_OF_EXP
+REFpath=/PATH/TO/REF/
+REFbase=dmel625
+
+# filters for insertion
+min_repl=2
+min_reads=4
+################################################################################
+################################################################################
+
+# change to wd
+cd $wd
+
+# create out10_combined.sorted.bed
+cat $path_to_PART1_output$SNa*"/"$SNa*"_out10_breakpoints.bed" | bedtools sort -i - > $SNa"_in10_combined.sorted.bed"
+
+# add a gene tag to each read (i.e. genomic location). this is done using a gene-only version of the gtf file.
+bedtools intersect -wa -a $SNa"_in10_combined.sorted.bed" -b $REFpath$REFbase"_GENES.bed" -loj -s > $SNa"_out01_genetagged.tsv"
+
+# separate | delimited field
+sed -e $'s/|/\t/g' $SNa"_out01_genetagged.tsv" > $SNa"_out02_sepparated.tsv" &&\
+	rm $SNa"_out01_genetagged.tsv"
+
+# generate column that contains the "basic" TE name i.e. TE_LTR ==> TE
+awk 'BEGIN {OFS = "\t"} {
+	a = $5
+	gsub(/_LTR/,"",a)
+	c = substr($4, 1, length($4)-2)
+	print $0"\t"a"\t"c
+	}' < $SNa"_out02_sepparated.tsv" > $SNa"_out03pre_TEbase.tsv" &&\
+		rm $SNa"_out02_sepparated.tsv"
+
+# remove duplicate reads (where both :A and :B version of the same reads were picked up
+awk '!seen[$21]++' $SNa"_out03pre_TEbase.tsv" > $SNa"_out03_TEbase.tsv" &&\
+	rm $SNa"_out03pre_TEbase.tsv"
+
+# create list of all TEs in dataset
+cut -f20 $SNa"_out03_TEbase.tsv" | sort | uniq > $SNa"_out03a_uniqueTEs.tsv"
+
+# to create collection file, first make sure this file does not yet exist
+rm -f $SNa"_out12_OUTPUT.tsv"
+
+# loop through all TEs in the dataset
+while read TE
+do
+	# grep TEs from combined data set
+	awk -v t="$TE" '{if ($20 == t) print $0;}' $SNa"_out03_TEbase.tsv" > "tmp."$SNa"_"$TE"_out04.tsv"
+	# split into TE-GENE and GENE-TE
+	awk '{if ($7 == "TE-GENE") print $0;}' "tmp."$SNa"_"$TE"_out04.tsv" > "tmp."$SNa"_"$TE"_out05_TE-GENE.tsv"
+	awk '{if ($7 == "GENE-TE") print $0;}' "tmp."$SNa"_"$TE"_out04.tsv" > "tmp."$SNa"_"$TE"_out05_GENE-TE.tsv"
+	# merge, counting unique read NAMES, and allowing for 20nt range of precise insertion site
+	# header: Chr(genome)|Start(genome)|End(genome)|TE|"."|Strand(genome)|NumberOfSamples|NumberOfReads|Breakpoint(genome)|Strand(TE)|TE-GENEorGENE-TE|GeneNames|AllBreakpoints(TE)
+	bedtools merge -i "tmp."$SNa"_"$TE"_out05_TE-GENE.tsv" -c 5,12,13,8,21,3,6,7,17,10 -o mode,mode,mode,count_distinct,count_distinct,mode,mode,mode,distinct,collapse -d 20 > "tmp."$SNa"_"$TE"_out06_TE-GENE.tsv"
+	bedtools merge -i "tmp."$SNa"_"$TE"_out05_GENE-TE.tsv" -c 5,12,13,8,21,3,6,7,17,10 -o mode,mode,mode,count_distinct,count_distinct,mode,mode,mode,distinct,collapse -d 20 > "tmp."$SNa"_"$TE"_out06_GENE-TE.tsv"
+	# filter to keep only insertions that were detected in at least $min_repl separate biological replicates with at least $min_reads reads
+	awk -v mreps="$min_repl" -v mreads="$min_reads" 'BEGIN {OFS = "\t"} {a = $0 ; if ( $7 >= mreps && $8 >= mreads) {print a}}' < "tmp."$SNa"_"$TE"_out06_TE-GENE.tsv" > "tmp."$SNa"_"$TE"_out07_TE-GENE.tsv"
+	awk -v mreps="$min_repl" -v mreads="$min_reads" 'BEGIN {OFS = "\t"} {a = $0 ; if ( $7 >= mreps && $8 >= mreads) {print a}}' < "tmp."$SNa"_"$TE"_out06_GENE-TE.tsv" > "tmp."$SNa"_"$TE"_out07_GENE-TE.tsv"
+	# combine TE-GENE and GENE-TE, then sort
+	cat "tmp."$SNa"_"$TE"_out07_TE-GENE.tsv" "tmp."$SNa"_"$TE"_out07_GENE-TE.tsv" | bedtools sort -i - > "tmp."$SNa"_"$TE"_out09.tsv"
+	# create list of all genes in ACHOR file
+	# first, replace "," with tab, then sort, then pick unique strings, then remove ""
+	cut -f12 "tmp."$SNa"_"$TE"_out09.tsv" | tr ',' '\n' | sort | uniq | sed '/\./d' > "tmp."$SNa"_"$TE"_out11_genes.tsv"
+	# loop through list of genes and print hits in genes to output
+	while read GENE
+	do
+		# create output file that contains for every gene with at least one anchor insertion, all the insertions of that TE
+		# ANCHOR hits have "yes" appended, others "NO"
+		awk -v g="$GENE" '{if ($12 ~ g) print g"\t"$1"\t"$6"\t"$9"\t"$4"\t"$11"\t"$10"\t"$13"\t"$7"\t"$8"\t""gDNA";}' "tmp."$SNa"_"$TE"_out09.tsv" >> $SNa"_out12_OUTPUT.tsv"
+	done < "tmp."$SNa"_"$TE"_out11_genes.tsv"
+	rm "tmp."$SNa*
+done < $SNa"_out03a_uniqueTEs.tsv"
+
+rm $SNa"_out03a_uniqueTEs.tsv"
+rm $SNa"_out03_TEbase.tsv"
+
+
+# now, every line is one incident of a gene-TE chimera
+while read line
+do
+	# convert line to bed format
+	# header: Chr(genome)\tStart(genome)[this is the breakpoint minus 1]\tEnd(genome)[this is the breakpoint]\tTE|Gene|TE-GENEorGENE-TE\t"."\tStrand(genome)
+	echo $line | awk '{ print $2"\t"$4-1"\t"$4"\t"$5"|"$1"|"$6"\t.\t"$3}' > "tmp."$SNa"_out13.bed"
+	
+	# $a is TE-GENE or GENE-TE	
+	a=$(echo $line | awk '{print $6}')
+	# $b is the gene
+	b=$(echo $line | awk '{print $1}')
+	
+	# intersect with FEATURES file to get all ovelapping features (output is ;-separated)
+	d=$(bedtools intersect -a "tmp."$SNa"_out13.bed" -b $REFpath$REFbase"_FEATURES.bed" -loj -wa | awk '{print $10}' | paste -sd ";" -)
+	
+	# the loop aboce created the unix variable $c. here, this variable is appended to the main file.
+	echo $line | awk -v d="$d" 'BEGIN {OFS=FS = "\t"} { print $0"\t.\t"d}'
+	rm "tmp."$SNa"_out13.bed"
+done < $SNa"_out12_OUTPUT.tsv" > $SNa"_out15.tsv" &&\
+	rm $SNa"_out12_OUTPUT.tsv"
+
+# for better readability, the concatenated breakpoints on the TE are pooled here (with the number of occurrences in brackets)
+cat $SNa"_out15.tsv" | while read line; do echo $line | awk '{print $8}' | awk -v RS=',' '{print$0}' | sort | uniq -c | awk '{if (NR!=1) printf$2"("$1"),"}'| sed 's/,$//'; done > $SNa"_newcolb"
+# for final output, get rid of original TE-breakpoint field
+awk 'BEGIN{FS=" ";OFS="\t"}{print $1,$2,$3,$4,$5,$6,$7,$9,$10,$11,$12,$13;}' $SNa"_out15.tsv" > $SNa"_newcola" &&\
+	rm $SNa"_out15.tsv"
+# append pooled TE breakpoints to final output
+paste $SNa"_newcola" $SNa"_newcolb" > $SNa"_chimericreads_gDNA_final.tsv"
+rm $SNa"_newcola" $SNa"_newcolb"
