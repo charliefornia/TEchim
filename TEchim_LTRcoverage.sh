6ab4e22e6a2e7f24684ce625549def5b1cca1e55
charliefornia <charliefornia@me.com>
2019-06-13 13:59:57 +0100

    updated input vars

--- TEchim_LTRcoverage.sh
+++ TEchim_LTRcoverage.sh
@@ -17,9 +17,9 @@
 ################################################################################
 ################################################################################
 # set parameters
-wd=~
-path_to_PART1_output=/PATH/TO/PART1/OUTPUT/
-REFpath=
+wd=$(pwd)
+path_to_PART1_output=$wd"/"
+REFpath=						# if left empty then REFpath from PART1 is used
 ################################################################################
 ################################################################################
 
@@ -75,26 +75,31 @@ do
 	list_of_lanes=$(find $path_to_PART1_output -maxdepth 1 -name "$SNa"_S"*" | rev | cut -d "/" -f 1 | awk '{gsub(/_/,"\t"); print $2"\t"$1}' | rev | grep $SNo | awk '{print $1}')
 	for LNo in $list_of_lanes
 	do
-		if [[ -f $SNa".LTRinput."$snum"_"$l".sam" ]]; then :; else samtools view $path_to_PART1_output$SNa"_"$snum"_"$l"_out4_Aligned.sortedByCoord.out.bam" | awk '{if ($3 ~ "TEchr_" && $7 != "=") print $0}' > $SNa".LTRinput."$snum"_"$l".sam"; fi
+		if [[ -f "tmp."$SNa".LTRinput."$SNo"_"$LNo".sam" ]]; then :; else samtools view $path_to_PART1_output$SNa"_"$SNo"_"$LNo"/"$SNa"_"$SNo"_"$LNo"_STAR/"$SNa"_"$SNo"_"$LNo"_out4_Aligned.sortedByCoord.out.bam" | awk '{if ($3 ~ "TEchr_" && $7 != "=") print $0}' > "tmp."$SNa".LTRinput."$SNo"_"$LNo".sam"; fi
 	done		
 done
+
+# write header
+echo TE$'\t'LTR-gene$'\t'LTR-TE > $SNa".LTR_TEs.proportion_of_TEvsGENE.tsv"
 
 while read line
 do
 	rm -f "tmp.output.collect_"$line
 	list_of_snum=$(find $path_to_PART1_output -maxdepth 1 -name "$SNa"_S"*" | rev | cut -d "/" -f 1 | awk '{gsub(/_/,"\t"); print $2}' | awk '!seen[$0]++ {print $0}' | rev)
 	for SNo in $list_of_snum
-		rm -f "tmp.output."$snum
+	do
+		rm -f "tmp.output."$SNo
 		list_of_lanes=$(find $path_to_PART1_output -maxdepth 1 -name "$SNa"_S"*" | rev | cut -d "/" -f 1 | awk '{gsub(/_/,"\t"); print $2"\t"$1}' | rev | grep $SNo | awk '{print $1}')
 		for LNo in $list_of_lanes
 		do
-			awk -v te="$line" '{if ($3 == "TEchr_"te"_LTR" && $7 == "TEchr_"te) counta++; else if ($3 == "TEchr_"te"_LTR" && $7 != "TEchr_"te && $7 != "=") countb++} END {print countb"\t"counta}' $SNa".LTRinput."$snum"_"$l".sam" >> "tmp.output."$snum
+			awk -v te="$line" '{if ($3 == "TEchr_"te"_LTR" && $7 == "TEchr_"te) counta++; else if ($3 == "TEchr_"te"_LTR" && $7 != "TEchr_"te && $7 != "=") countb++} END {print countb"\t"counta}' "tmp."$SNa".LTRinput."$SNo"_"$LNo".sam" >> "tmp.output."$SNo
 		done
-		awk '{suma+=$1; sumb+=$2;} END {print suma"\t"sumb;}' "tmp.output."$snum >> "tmp.output.collect_"$line
-		rm -f "tmp.output."$snum		
+		awk '{suma+=$1; sumb+=$2;} END {print suma"\t"sumb;}' "tmp.output."$SNo >> "tmp.output.collect_"$line
+		rm -f "tmp.output."$SNo		
 	done
 	a=$(cat "tmp.output.collect_"$line | awk '{print $1}' | paste -sd "|" -)
 	b=$(cat "tmp.output.collect_"$line | awk '{print $2}' | paste -sd "|" -)
 	echo $line $a $b
 	rm -f "tmp.output.collect_"$line
-done < $REFpath$TElist".list_of_LTR_TE_names.txt" > $SNa".LTR_TEs.proportion_of_TEvsGENE.tsv"
+done < $REFpath$TElist".list_of_LTR_TE_names.txt" >> $SNa".LTR_TEs.proportion_of_TEvsGENE.tsv"
+rm "tmp."$SNa".LTRinput."*
