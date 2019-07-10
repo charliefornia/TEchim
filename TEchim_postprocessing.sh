d7dae3395a243e06092e784907673055e8070e13
charliefornia <charliefornia@me.com>
2019-07-10 16:31:49 +0100

    added section for TEs

--- TEchim_postprocessing.sh
+++ TEchim_postprocessing.sh
@@ -72,3 +72,22 @@ done
 wait
 
 
+cd $wd
+awk '{print $2"\t"$4-21"\t"$4+20"\tLINE"NR"\t.\t"$3}' $SNa"_TE_chimericreads_final.withGENES.ABOVE1.tsv" > $SNa"_TE_chimericreads_final.STEP4.bed"
+list_of_SNo=$(find $path_to_PART1_output -maxdepth 1 -name "$SNa"_S"*" | rev | cut -d "/" -f 1 | awk '{gsub(/_/,"\t"); print $2}' | awk '!seen[$0]++ {print $0}' | rev)
+for SNo in $list_of_SNo
+do
+	list_of_LNo=$(find $path_to_PART1_output -maxdepth 1 -name "$SNa"_S"*" | rev | cut -d "/" -f 1 | awk '{gsub(/_/,"\t"); print $2"\t"$1}' | rev | grep $SNo | awk '{print $1}')
+	for LNo in $list_of_LNo
+	do
+		bedtools multicov -bams $path_to_PART1_output$SNa"_"$SNo"_"$LNo"/"$SNa"_"$SNo"_"$LNo"_STAR/"$SNa"_"$SNo"_"$LNo"_out4_Aligned.sortedByCoord.out.bam" -bed $SNa"_TE_chimericreads_final.STEP4.bed" -s  | awk '{print $7}' > "tmp."$SNa"_"$SNo"_"$LNo"_TE_insertionsites.coverage_perSample.tsv" &
+	done
+done
+wait
+paste "tmp."$SNa"_"*"_TE_insertionsites.coverage_perSample.tsv" | awk '{sum=0; for(i=1; i<=NF; i++){sum+=$i}; sum/=NF; printf "%.0f\n",sum}' > "tmp."$SNa"_TEcoverage_averages.tsv"
+# combine averages with chimeric output
+paste $SNa"_TE_chimericreads_final.withGENES.ABOVE1.tsv" "tmp."$SNa"_TEcoverage_averages.tsv" > $SNa"_TEref_chimericreads_final.STEP4.tsv"
+
+
+
+
