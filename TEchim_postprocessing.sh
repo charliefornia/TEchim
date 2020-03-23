f7cef10b8747296cda79ecb667bd3851b829e5d0
charliefornia <charliefornia@me.com>
2020-03-23 11:02:07 +0000

    added comments to code

--- TEchim_postprocessing.sh
+++ TEchim_postprocessing.sh
@@ -56,18 +56,23 @@ do
 	while read line; do a=$(echo $line | awk '{print$1}'); b=$(echo $line | awk '{gsub ("@","\t"); print $5}'); c=$(grep $a $paralogs | grep $b | wc -l | awk '{print $1}'); if [[ $c = "0" ]]; then echo $line | awk '{gsub(" ","\t"); print $0}'; fi; done < "pp.tmp."$IGEgroup"_"$SNa"_IGEref_chimericreads_final.STEP1" > "pp.tmp."$IGEgroup"_"$SNa"_IGEref_chimericreads_final.STEP2"
 	# remove cases where the hit is within +/- 5kb of IGE
 	awk '{ orig=$0; gsub("@","\t",$5); a=$5; gsub("-","\t",a); $0=a; gsub("\\(","\t",$4); gsub(":","\t"); chr=$2; start=$3-5000; end=$4+5000; $0=orig; if($2!=chr) print $0; else if (start > $4 || end < $4) print $0}' "pp.tmp."$IGEgroup"_"$SNa"_IGEref_chimericreads_final.STEP2" > "pp.tmp."$IGEgroup"_"$SNa"_IGEref_chimericreads_final.STEP3"	
+	# create bed with +/- 20nt extensions for assessing coverage around breakpoint
 	awk '{print $2"\t"$4-21"\t"$4+20"\tLINE"NR"\t.\t"$3}' "pp.tmp."$IGEgroup"_"$SNa"_IGEref_chimericreads_final.STEP3" > "pp.tmp."$IGEgroup"_"$SNa"_IGEref_chimericreads_final.STEP4.bed"
+	# iterate through replicates
 	list_of_SNo=$(find $path_to_PART1_output -maxdepth 1 -name "$SNa"_S"*" | rev | cut -d "/" -f 1 | awk '{gsub(/_/,"\t"); print $2}' | awk '!seen[$0]++ {print $0}' | rev)
 	for SNo in $list_of_SNo
 	do
+		# iterate through lanes
 		list_of_LNo=$(find $path_to_PART1_output -maxdepth 1 -name "$SNa"_S"*" | rev | cut -d "/" -f 1 | awk '{gsub(/_/,"\t"); print $2"\t"$1}' | rev | grep $SNo | awk '{print $1}')
 		for LNo in $list_of_LNo
 		do
+			# get coverage around each breakpoint - here strand-specific
 			bedtools multicov -bams $path_to_PART1_output$SNa"_"$SNo"_"$LNo"/"$SNa"_"$SNo"_"$LNo"_STAR/"$SNa"_"$SNo"_"$LNo"_out4_Aligned.sortedByCoord.out.bam" -bed "pp.tmp."$IGEgroup"_"$SNa"_IGEref_chimericreads_final.STEP4.bed" -s  | awk '{print $7}' > "pp.tmp."$IGEgroup"_"$SNa"_"$SNo"_"$LNo"_insertionsites.coverage_perSample.tsv" &
 		done
 	done
 	wait
 	rm "pp.tmp."$IGEgroup"_"$SNa"_IGEref_chimericreads_final.STEP4.bed"
+	# combine coverages from each lane/replicate and take average value
 	paste "pp.tmp."$IGEgroup"_"$SNa"_"*"_insertionsites.coverage_perSample.tsv" | awk '{sum=0; for(i=1; i<=NF; i++){sum+=$i}; sum/=NF; printf "%.0f\n",sum}' > "pp.tmp."$IGEgroup"_"$SNa"_IGEcoverage_averages.tsv"
 	rm "pp.tmp."$IGEgroup"_"$SNa"_"*"_insertionsites.coverage_perSample.tsv"
 	# combine averages with chimeric output
@@ -83,24 +88,32 @@ wait
 
 # for TEs
 cd $wd
+# remove hits in less than 1 sample
 awk '{if ($8 > 1) {print "LINE"NR"\t"$0}}' $SNa"_TE_chimericreads_final_withGENEreads.tsv" > "pp.tmp."$SNa"_TE_chimericreads_final.ABOVE1.STEP3.tsv"
+# create bed with +/- 20nt extensions for assessing coverage around breakpoint
 awk '{print $3"\t"$5-21"\t"$5+20"\t"$1"\t.\t"$4}' "pp.tmp."$SNa"_TE_chimericreads_final.ABOVE1.STEP3.tsv" > "pp.tmp."$SNa"_TE_chimericreads_final.STEP4.bed"
+# iterate through replicates
 list_of_SNo=$(find $path_to_PART1_output -maxdepth 1 -name "$SNa"_S"*" | rev | cut -d "/" -f 1 | awk '{gsub(/_/,"\t"); print $2}' | awk '!seen[$0]++ {print $0}' | rev)
 for SNo in $list_of_SNo
 do
+	# iterate through lanes
 	list_of_LNo=$(find $path_to_PART1_output -maxdepth 1 -name "$SNa"_S"*" | rev | cut -d "/" -f 1 | awk '{gsub(/_/,"\t"); print $2"\t"$1}' | rev | grep $SNo | awk '{print $1}')
 	for LNo in $list_of_LNo
 	do
+		# get coverage around each breakpoint - here strand-specific
 		bedtools multicov -bams $path_to_PART1_output$SNa"_"$SNo"_"$LNo"/"$SNa"_"$SNo"_"$LNo"_STAR/"$SNa"_"$SNo"_"$LNo"_out4_Aligned.sortedByCoord.out.bam" -bed "pp.tmp."$SNa"_TE_chimericreads_final.STEP4.bed" -s  | awk '{print $7}' > "pp.tmp."$SNa"_"$SNo"_"$LNo"_TE_insertionsites.coverage_perSample.tsv" &
 	done
 done
 wait
 rm "pp.tmp."$SNa"_TE_chimericreads_final.STEP4.bed"
+# combine coverages from each lane/replicate and take average value
 paste "pp.tmp."$SNa"_"*"_TE_insertionsites.coverage_perSample.tsv" | awk '{sum=0; for(i=1; i<=NF; i++){sum+=$i}; sum/=NF; printf "%.0f\n",sum}' > "pp.tmp."$SNa"_TEcoverage_averages.tsv"
 rm "pp.tmp."$SNa"_"*"_TE_insertionsites.coverage_perSample.tsv"
+# combine with original file
 paste "pp.tmp."$SNa"_TE_chimericreads_final.ABOVE1.STEP3.tsv" "pp.tmp."$SNa"_TEcoverage_averages.tsv" | cut -f 2- > $SNa"_TE_chimericreads_final.withGENES.ABOVE1.withAverages.tsv"
 rm "pp.tmp."$SNa"_TEcoverage_averages.tsv"
 rm "pp.tmp."$SNa"_TE_chimericreads_final.ABOVE1.STEP3.tsv"
+# re-add pre-filtered lines - this is to find rare splice-variants that were only detected in a single replicate - need "anchor hit".
 awk '{if ($8 <= 1) {print $0}}' $SNa"_TE_chimericreads_final_withGENEreads.tsv" | cat - $SNa"_TE_chimericreads_final.withGENES.ABOVE1.withAverages.tsv" > $SNa"_TE_chimericreads_final.withGENES.ALL.withAverages.tsv"
 
 
